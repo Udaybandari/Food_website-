fa8cb08f11d23b69ed7834d05582b0715d1bda0c
Uday bandari <udaybandari593@gmail.com>
2025-07-05 22:06:25 +0530

    updated the herocomponent feature

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react' 
 import myimage from '../../assets/vector3.png'
+import myimage2 from '../../assets/biryani2.png'
 const bgimage=
   {
    backgroundImage:`url(${myimage})`,
@@ -13,8 +14,18 @@ const bgimage=
 const Hero = () => {
   
   return (
-    <div style={bgimage}  className='sm:min-h-[600px] bg-gray-100  dark:bg-gray-900 dark:text-white duration-200 flex justify-center items-center '>
-      <h1>Hello </h1>
+    <div style={bgimage}  className='   sm:min-h-[600px] bg-white  dark:bg-gray-900 dark:text-white duration-200 flex justify-center items-center '>
+      <div className='grid grid-cols-2 place-items-center    '>
+        <div  className='flex flex-col justify-between items-start space-y-3 gap-2'>
+          <h1 className='font-bold  text-7xl w-[220px]'>Welcome to the Foodie Zone
+          </h1>
+          <p className='line-clamp-3 w-99'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis blanditiis unde repellat quidem quam. Culpa eaque sint aliquid tenetur saepe!</p>
+          <button className='outlinebutton'>order now</button>
+        </div> 
+        <div>
+       <img src={myimage2} className='w-[352px] spin' alt="" />
+        </div>
+      </div>
     </div>
   )
 }
